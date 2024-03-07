a9b6da5cd62812714a91f3ad2170abd137374601
Jay Dinesh Rane <jayu.rane32003@gmail.com>
2024-03-07 21:04:41 +0530

    Files Added without node modules

---
+++ my-app/frontend/src/component/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Tab from './table'
+
+const Home = () => {
+   
+  return (
+    <div style={{ textAlign: 'center' }}>
+    <div style={{ display: 'inline-block' }}>
+      <Tab />
+    </div>
+  </div>
+  )
+}
+
+export default Home
